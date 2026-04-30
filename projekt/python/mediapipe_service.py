1e739b9893f6728d97d628c060d4722acbe55b46
Windgun <marczak.m2005@gmail.com>
2026-04-30 03:46:11 +0200

    wstępny szkielet pod asynchroniczne przetwarzanie obrazu biblioteką mediapipe

--- projekt/python/mediapipe_service.py
+++ projekt/python/mediapipe_service.py
@@ -1,25 +1,38 @@
 import asyncio
+import sys
+
 import websockets
 import json
 import cv2
 import base64
+
+import mediapipe as mp
 
 
 ### CONSTANTS
 camera_refresh_rate = 10
 points_refresh_rate = 10
 
+mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
+
+pose = mp_pose.Pose(
+    model_complexity=2,
+    smooth_landmarks=True,
+    min_detection_confidence=0.7,
+    min_tracking_confidence=0.7
+)
 
 # global cache variables
-frame = None # last captured camera image
+cap = None # last captured camera image
 points = None # last calculated landmarks by mediapipe
 
 async def send_camera_data(websocket):
-    global frame
-    frame = cv2.VideoCapture(0)
+    global cap
+    cap = cv2.VideoCapture(0)
 
     while True:
-        ret, frame = frame.read()
+        ret, frame = cap.read()
         if not ret:
             continue
 
@@ -28,20 +41,36 @@ async def send_camera_data(websocket):
         jpg_as_text = base64.b64encode(buffer).decode('utf-8')
 
         data = {
-            "image": jpg_as_text
+            "image": jpg_as_text,
+            "points": points
         }
 
         await websocket.send(json.dumps(data))
         await asyncio.sleep(1 / camera_refresh_rate)
 
+def generate_points():
+    global cap, points
+
+    # rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # results = pose.process(rgb)
+    return
+
+async def recalc_points():
+    while True:
+        generate_points()
+        await asyncio.sleep(1 / points_refresh_rate)
 
 async def handler(websocket):
     print("Electron connected")
     await send_camera_data(websocket)
 
+async def start_server():
+    # Use websockets.serve as a context manager or await it
+    async with websockets.serve(handler, "localhost", 8765):
+        await asyncio.Future()  # This keeps the server running forever
+
 async def main():
-    start_server = websockets.serve(handler, "localhost", 8765)
-    await start_server
+    await asyncio.gather(start_server(), recalc_points())
     await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
